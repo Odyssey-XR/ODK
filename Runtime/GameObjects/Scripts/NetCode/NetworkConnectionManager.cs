e8a6c70b5d30fff44d8d4008226467acd5a47878
NaAquelo <Joshualogananderson@gmail.com>
2024-05-18 12:05:41 +0100

    doc: Adds comments to connection manager

--- Runtime/GameObjects/Scripts/NetCode/NetworkConnectionManager.cs
+++ Runtime/GameObjects/Scripts/NetCode/NetworkConnectionManager.cs
@@ -1,9 +1,14 @@
 namespace ODK.GameObjects.NetCode
 {
   using Unity.Netcode;
+  using UnityEngine;
 
+  /// <summary>
+  /// A network manager that automatically starts a host or client based on the build target.
+  /// </summary>
   public class NetworkConnectionManager : NetworkManager
   {
+    /// <inheritdoc cref="MonoBehaviour"/>
     protected void Start()
     {
 #if UNITY_EDITOR
